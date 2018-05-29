2272056fd6f74493929d5fa03aacad2a4871cf1d
xiaobo.yang <tigerQAZ2@>
2018-05-29 16:09:32 +0800

    1.服务注册和发现

---
+++ eurekaclienttest/src/main/java/com/frank/clienttest/Application.java
@@ -0,0 +1,18 @@
+package com.frank.clienttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author franyang
+ * @date 2018/5/29
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ eurekaserver/src/main/java/com/frank/server/ServerApplication.java
@@ -0,0 +1,20 @@
+package com.frank.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author franyang
+ * @date 2018/5/10 15:40
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
