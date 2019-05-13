c99fc3c3fcf2c13ccdafe21c2c6241264acc553f
yangxiaobo <Xiaobo.Yang5@geely.com>
2019-05-13 17:09:29 +0800

    单机注册中心添加权限校验

--- eurekaserver/src/main/java/com/frank/server/ServerApplication.java
+++ eurekaserver/src/main/java/com/frank/server/ServerApplication.java
@@ -3,15 +3,39 @@ package com.frank.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author franyang
  * @date 2018/5/10 15:40
  */
 @EnableEurekaServer
+@EnableWebSecurity
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().httpBasic();
+        super.configure(http);
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        User.UserBuilder builder = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        // 必须添加roles,否则报错
+        manager.createUser(builder.username("admin").password("admin").roles("USER").build());
+
+        return manager;
     }
 }
